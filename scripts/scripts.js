d01acb20ccaa7b95ef2fea89295b37071ff9bb93
Nathan Hungate <Nathan.J.Hungate@gmail.com>
2017-02-22 11:29:05 -0700

    Add click handler for mobile nav menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,29 +1,12 @@
-const debounce = function(func, wait, immediate) {
-  var timeout;
-  return function() {
-    var context = this, args = arguments;
-    var later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
+const $toggleButton = document.querySelector('.nav-toggle');
+const $navMenu = document.querySelector('.nav-menu');
+
+$toggleButton.onclick = (e) => {
+  $toggleButton.classList.toggle('is-active');
+  $navMenu.classList.toggle('is-active');
 };
 
-const adjustPadding = function() {
-  const $header = document.querySelector('header > .section');
-  const windowHeight = window.innerHeight;
-  const headerComputedHeight = window.getComputedStyle($header, null).getPropertyValue('height');
-  const headerHeight = Number.parseInt(headerComputedHeight, 10);
-
-  $header.style.paddingTop = ((windowHeight - headerHeight) / 2) + 'px';
-  $header.style.paddingBottom = ((windowHeight - headerHeight) / 2) + 'px';
+$navMenu.onclick = (e) => {
+  $toggleButton.classList.remove('is-active');
+  $navMenu.classList.remove('is-active');
 };
-
-const throttleAdjustPadding = debounce(adjustPadding, 200);
-
-adjustPadding();
-window.addEventListener('resize', throttleAdjustPadding);
